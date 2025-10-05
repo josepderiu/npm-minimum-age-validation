6a2cce3d7d4b06c6d19e6b216becb8e09c8ae0b9
mutaito <jderiu@gmail.com>
2025-10-05 22:38:04 +0200

    fix: enhance Codecov upload step and update Jest coverage reporters

--- jest.config.js
+++ jest.config.js
@@ -2,32 +2,22 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/__tests__'],
-  testMatch: [
-    '**/*.test.ts'
-  ],
+  testMatch: ['**/*.test.ts'],
   transform: {
-    '^.+\\.ts$': 'ts-jest'
+    '^.+\\.ts$': 'ts-jest',
   },
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/**/*.d.ts',
-    '!src/cli.ts'
-  ],
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts', '!src/cli.ts'],
   coverageDirectory: 'coverage',
-  coverageReporters: [
-    'text',
-    'lcov',
-    'html'
-  ],
+  coverageReporters: ['text', 'lcov', 'html', 'json'],
   coverageThreshold: {
     global: {
       branches: 30,
       functions: 50,
       lines: 40,
-      statements: 40
-    }
+      statements: 40,
+    },
   },
   setupFilesAfterEnv: ['<rootDir>/__tests__/setup.ts'],
   testTimeout: 10000,
-  verbose: true
+  verbose: true,
 };
